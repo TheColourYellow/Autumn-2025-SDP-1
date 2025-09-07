28d07edc53973a0f0cced335cbac830314a00a2f
TaruNipuli <taru.nipuli@metropolia.fi>
2025-09-07 14:25:00 +0300

    Add view folder and separate BookstoreView class

--- src/main/java/com/group9/GuiTestMain.java
+++ src/main/java/com/group9/GuiTestMain.java
@@ -1,34 +1,13 @@
+
 package com.group9;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import com.group9.view.BookstoreView;
 
-import java.util.Objects;
-
-public class GuiTestMain extends Application {
-
-    // For testing the JavaFX user interface
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Load the FXML layout for the bookstore_view.fmxl
-        Parent root = FXMLLoader.load(Objects.requireNonNull(
-                getClass().getResource("/bookstore_view.fxml")
-        ));
-
-        // Set the window title
-        primaryStage.setTitle("Bookstore Management System");
-
-        // Set the scene and show the window
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
+public class GuiTestMain {
     public static void main(String[] args) {
-        launch(args);
+        BookstoreView.launch(BookstoreView.class, args);
     }
 }
+
+
 
