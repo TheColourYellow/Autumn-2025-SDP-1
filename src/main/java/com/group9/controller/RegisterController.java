9fcb3ab9524834cad96212c5e554bcaefaf2cf9e
TaruNipuli <taru.nipuli@metropolia.fi>
2025-09-14 14:53:40 +0300

    Add switching between login_view to register_view

--- src/main/java/com/group9/controller/RegisterController.java
+++ src/main/java/com/group9/controller/RegisterController.java
@@ -9,45 +9,24 @@ import javafx.stage.Stage;
 
 import static com.group9.util.PopupUtils.showError;
 
-public class LoginController {
+public class RegisterController {
 
     @FXML
     private Label homeLabel;
-
-    @FXML
-    private Label registerLabel;
 
     @FXML
     private void openHomeWindow() {
         try {
             // Load the FXML file for the home window
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/bookstore_view.fxml"));
-            Parent loginRoot = loader.load();
+            Parent root = loader.load();
 
             Stage stage = (Stage) homeLabel.getScene().getWindow();
-
-            stage.setScene(new Scene(loginRoot));
-
+            stage.setScene(new Scene(root));
             stage.setTitle("Bookstore Management System");
             stage.show();
         } catch (Exception e) {
             showError("Error", "Could not open home window");
         }
     }
-
-    @FXML
-    private void openRegisterWindow() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/register_view.fxml"));
-            Parent root = loader.load();
-
-            Stage stage = (Stage) registerLabel.getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.setTitle("Register");
-            stage.show();
-        } catch (Exception e) {
-            showError("Error", "Could not open register window.");
-        }
-    }
 }
-
