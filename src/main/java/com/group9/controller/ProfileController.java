ca19d7fa86b8a4bb9c7ad336cc076436032aa192
TaruNipuli <taru.nipuli@metropolia.fi>
2025-09-19 13:13:09 +0300

    Add ProfileContorller with functionality to use Home label

---
+++ src/main/java/com/group9/controller/ProfileController.java
@@ -0,0 +1,33 @@
+package com.group9.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import static com.group9.util.PopupUtils.showError;
+
+public class ProfileController {
+
+    @FXML
+    private Label homeLabel;
+
+    @FXML
+    private void openHomeWindow() {
+        try {
+            // Load the FXML file for the home window
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/bookstore_view.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) homeLabel.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Bookstore Management System");
+            stage.show();
+        } catch (Exception e) {
+            showError("Error", "Could not open home window");
+        }
+    }
+}
+
