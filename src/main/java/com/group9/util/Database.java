4ff3c785187a2e683b2b78c81cf7403d520c4ac4
Vvanttinen <veeti.vanttinen@metropolia.fi>
2025-10-06 21:20:49 +0300

    Database update to use default values if env is null

--- src/main/java/com/group9/util/Database.java
+++ src/main/java/com/group9/util/Database.java
@@ -7,9 +7,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-  private static final String URL = System.getenv("DATABASE_URL");
-  private static final String USER = System.getenv("DATABASE_USER");
-  private static final String PASSWORD = System.getenv("DATABASE_PASSWORD");
+  // Default values if environment variables are not set
+  private static final String DEFAULT_URL = "jdbc:mariadb://localhost:3306/bookstore";
+  private static final String DEFAULT_USER = "bookstore_user";
+  private static final String DEFAULT_PASSWORD = "bookstore_pass";
+
+  private static final String URL = System.getenv().getOrDefault("DATABASE_URL", DEFAULT_URL);
+  private static final String USER = System.getenv().getOrDefault("DATABASE_USER", DEFAULT_USER);
+  private static final String PASSWORD = System.getenv().getOrDefault("DATABASE_PASSWORD", DEFAULT_PASSWORD);
 
   public static Connection getConnection() throws SQLException {
     return DriverManager.getConnection(URL, USER, PASSWORD);
