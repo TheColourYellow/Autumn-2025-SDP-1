e3dbd1d86490e651f90ef5390eec018ca3d89280
CrimsonBambi <gerli.hirv@metropolia.fi>
2025-11-26 15:19:52 +0200

    changed constructor as protected

--- src/main/java/com/group9/model/BookAttribute.java
+++ src/main/java/com/group9/model/BookAttribute.java
@@ -5,7 +5,7 @@ public abstract class BookAttribute {
   private String name;
   private String description;
 
-  public BookAttribute(int id, String name, String description) {
+  protected BookAttribute(int id, String name, String description) {
     this.id = id;
     this.name = name;
     this.description = description;
